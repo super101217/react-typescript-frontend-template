625240d7ed723a0108bde1036f0f28fbfd5e17a9
Superstar0107 <am000hw@gmail.com>
2023-06-12 11:22:52 -0400

    feat: update header & footer

--- src/components/common/Layout/Footer/index.tsx
+++ src/components/common/Layout/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const FooterSection: React.FC = () => {
   return (
     <footer className="flex justify-center p-4 text-gray-100 bg-gray-800 w-full fixed bottom-0">
-      <h1 className="text-xl">Designed by Steven K. Fish</h1>
+      <h1 className="text-xl">React + Typescript Frontend Template</h1>
     </footer>
   );
 };
--- src/components/view/Dashboard/index.tsx
+++ src/components/view/Dashboard/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const DashboardComponent: React.FC = () => {
-  return <h1>Welcome to Dashboard!!!</h1>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h1>Welcome to Dashboard!!!</h1>
+    </div>
+  );
 };
