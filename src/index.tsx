37e64a7756429faf3c679f83d77ccac8d917b16d
Superstar0107 <am000hw@gmail.com>
2023-08-21 23:27:38 +0900

    feat: add styles to asset

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/index.css';
 
 // create react node
 const root = ReactDOM.createRoot(
