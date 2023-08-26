40a823d6075d4056bed11b348da8305ba321eedd
Superstar0107 <am000hw@gmail.com>
2023-08-27 02:10:05 +0900

    feat: update sort order config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import './assets/styles/index.css';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import '@radix-ui/themes/styles.css';
 
