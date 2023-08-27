51f8124efc7e66a184a5435126f0d1b7fed011df
Superstar0107 <am000hw@gmail.com>
2023-08-27 23:37:52 +0900

    feat: update config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import './assets/styles/index.css';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import '@radix-ui/themes/styles.css';
+import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 // create react node
